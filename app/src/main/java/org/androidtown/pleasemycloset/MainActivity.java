0427909c288bea1d99b3f74c54ce1e521a13bc5d
ybib <yb359kr@gamail.com>
2019-08-08 17:45:24 +0900

    basic layout and camera func

--- app/src/main/java/org/androidtown/pleasemycloset/MainActivity.java
+++ app/src/main/java/org/androidtown/pleasemycloset/MainActivity.java
@@ -1,7 +1,10 @@
 package org.androidtown.pleasemycloset;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnSeeMyCloset = (Button) findViewById(R.id.btnSeeMyCloset);
+        Button btnSeeMyFriend = (Button) findViewById(R.id.btnSeeMyFriend);
+
+        btnSeeMyCloset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainIntent = new Intent(MainActivity.this, MyClosetActivity.class);
+                MainActivity.this.startActivity(mainIntent);
+            }
+        });
     }
 }
